e50cd4f80da48c1d0011bc66d9ffe5713c885982
Daniel T <ghostdanyt.a@gmail.com>
2021-12-15 13:34:03 -0400

    (FIX) PR changes done

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -7,37 +7,44 @@ class StringCalculatorTest(TestCase):
     def setUp(self):
         self.string_calculator = StringCalculator()
 
-    def test_string_calculator_zero(self):
-        test_string = ''
-        expected_number = 0
-        self.assertEqual(self.string_calculator.add(test_string), expected_number)
+    def test_string_calculator_empty_string_as_input(self):
+        input_test_string = ''
+        expected_number_response = 0
+        response_number = self.string_calculator.add(input_test_string)
+        self.assertEqual(response_number, expected_number_response)
     
     def test_string_calculator_one_value(self):
-        test_string = '3'
-        expected_number = 3
-        self.assertEqual(self.string_calculator.add(test_string), expected_number)
+        input_test_string = '3'
+        expected_number_response = 3
+        response_number = self.string_calculator.add(input_test_string)
+        self.assertEqual(response_number, expected_number_response)
 
     def test_string_calculator_two_values_delimited(self):
-        test_string = '10,20'
-        expected_number = 30
-        self.assertEqual(self.string_calculator.add(test_string), expected_number)
+        input_test_string = '10,20'
+        expected_number_response = 30
+        response_number = self.string_calculator.add(input_test_string)
+        self.assertEqual(response_number, expected_number_response)
 
     def test_string_calculator_two_lines_values(self):
-        test_string = '1\n2'
-        expected_number = 3
-        self.assertEqual(self.string_calculator.add(test_string), expected_number)
+        input_test_string = '1\n2'
+        expected_number_response = 3
+        response_number = self.string_calculator.add(input_test_string)
+        self.assertEqual(response_number, expected_number_response)
 
     def test_string_calculator_two_lines_values_delimited(self):
-        test_string = '1\n2,3\n4'
-        expected_number = 10
-        self.assertEqual(self.string_calculator.add(test_string), expected_number)
+        input_test_string = '1\n2,3\n4'
+        expected_number_response = 10
+        response_number = self.string_calculator.add(input_test_string)
+        self.assertEqual(response_number, expected_number_response)
     
     def test_string_calculator_negative_values_delimited(self):
-        test_string = '-1,2,-3'
+        input_test_string = '-1,2,-3'
         expected_value_raised = 'negatives not allowed: -1,-3'
         with self.assertRaises(Exception) as context:
-            self.string_calculator.add(test_string)
+            self.string_calculator.add(input_test_string)
         self.assertTrue(expected_value_raised in str(context.exception))
 
-
-unittest.main(argv=[''],verbosity=2, exit=False)
+# Verbosity lvl2 lets us get the help string of each test and the result
+# on the first log's lines for better understanding when some tests fail
+# even when all tests pass
+unittest.main(verbosity=2)
