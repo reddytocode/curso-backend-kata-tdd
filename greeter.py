15e36d6fb21f4b3975e02bb55c868bd0eb0d3bd9
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-12-14 20:43:25 -0400

    4th task completed

--- greeter.py
+++ greeter.py
@@ -1,6 +1,11 @@
+from datetime import datetime
 
 
 class Greeter:
     def greet(self, name):
         name = name.strip().capitalize()
+        now = datetime.now()
+        hour = now.hour
+        if 6 < hour < 12:
+            return f"Good morning {name}"
         return f"Hello {name}"
--- test_greeter.py
+++ test_greeter.py
@@ -1,21 +1,31 @@
 import unittest
 from greeter import Greeter
+from freezegun import freeze_time
 
 
 class GreeterTest(unittest.TestCase):
+    @freeze_time("2012-01-01 07:45:01")
     def test_greet(self):
         test_name = "Daniel"
-        expected_greet = f"Hello {test_name}"
+        expected_greet = f"Good morning {test_name}"
         self.assertEqual(Greeter().greet(test_name), expected_greet)
 
+    @freeze_time("2012-01-01 07:45:01")
     def test_greet_trims_input(self):
         test_name = "    Raquel "
-        expected_greet = "Hello Raquel"
+        expected_greet = "Good morning Raquel"
         self.assertEqual(Greeter().greet(test_name), expected_greet)
 
+    @freeze_time("2012-01-01 07:45:01")
     def test_greet_capitalize_input(self):
         test_name = "sebas"
-        expected_greet = "Hello Sebas"
+        expected_greet = "Good morning Sebas"
+        self.assertEqual(Greeter().greet(test_name), expected_greet)
+
+    @freeze_time("2012-01-01 07:45:01")
+    def test_greet_good_morning(self):
+        test_name = "Daniel"
+        expected_greet = f"Good morning {test_name}"
         self.assertEqual(Greeter().greet(test_name), expected_greet)
 
 
