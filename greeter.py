6ec5795a205b94c84125c8aef0d0f07bdb3ba007
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-12-14 20:49:41 -0400

    complete kata

--- greeter.py
+++ greeter.py
@@ -6,6 +6,10 @@ class Greeter:
         name = name.strip().capitalize()
         now = datetime.now()
         hour = now.hour
-        if 6 < hour < 12:
+        if 6 <= hour <= 12:
             return f"Good morning {name}"
+        elif 18 <= hour < 22:
+            return f"Good evening {name}"
+        elif 22 <= hour <= 23 or 0 <= hour < 6:
+            return f"Good night {name}"
         return f"Hello {name}"
--- test_greeter.py
+++ test_greeter.py
@@ -4,28 +4,40 @@ from freezegun import freeze_time
 
 
 class GreeterTest(unittest.TestCase):
-    @freeze_time("2012-01-01 07:45:01")
+    @freeze_time("2012-01-01 13:45:01")
     def test_greet(self):
         test_name = "Daniel"
-        expected_greet = f"Good morning {test_name}"
+        expected_greet = f"Hello {test_name}"
         self.assertEqual(Greeter().greet(test_name), expected_greet)
 
-    @freeze_time("2012-01-01 07:45:01")
+    @freeze_time("2012-01-01 13:45:01")
     def test_greet_trims_input(self):
         test_name = "    Raquel "
-        expected_greet = "Good morning Raquel"
+        expected_greet = "Hello Raquel"
         self.assertEqual(Greeter().greet(test_name), expected_greet)
 
-    @freeze_time("2012-01-01 07:45:01")
+    @freeze_time("2012-01-01 13:45:01")
     def test_greet_capitalize_input(self):
         test_name = "sebas"
-        expected_greet = "Good morning Sebas"
+        expected_greet = "Hello Sebas"
         self.assertEqual(Greeter().greet(test_name), expected_greet)
 
     @freeze_time("2012-01-01 07:45:01")
     def test_greet_good_morning(self):
         test_name = "Daniel"
         expected_greet = f"Good morning {test_name}"
+        self.assertEqual(Greeter().greet(test_name), expected_greet)
+
+    @freeze_time("2012-01-01 19:45:01")
+    def test_greet_good_evening(self):
+        test_name = "Daniel"
+        expected_greet = f"Good evening {test_name}"
+        self.assertEqual(Greeter().greet(test_name), expected_greet)
+
+    @freeze_time("2012-01-01 00:45:01")
+    def test_greet_good_night(self):
+        test_name = "Daniel"
+        expected_greet = f"Good night {test_name}"
         self.assertEqual(Greeter().greet(test_name), expected_greet)
 
 
